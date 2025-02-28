5d2f755c48c337e901a4cda27baf450dae7e7147
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2025-02-28 14:34:40 +0545

    login support & delete files

--- src/anonfile/exceptions.py
+++ src/anonfile/exceptions.py
@@ -1,15 +1,19 @@
-class AnofileError(Exception):
+class AnonfileError(Exception):
     """Base exception for Anonfile errors."""
     pass
 
-class FileNotFoundError(AnofileError):
+class FileNotFoundError(AnonfileError):
     """Exception raised when a file is not found."""
     pass
 
-class TimeoutError(AnofileError):
+class TimeoutError(AnonfileError):
     """Exception raised when the request times out."""
     pass
 
-class ConnectionError(AnofileError):
+class ConnectionError(AnonfileError):
     """Exception raised when there is a connection error."""
+    pass
+
+class JsonDecodeError(AnonfileError):
+    """Exception raised when there is a JsonDecode error."""
     pass
